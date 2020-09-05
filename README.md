7a82351a4e031221b370c05ed3b5f41de11d56dd
lulzsun <jminquach@gmail.com>
2020-09-05 07:37:34 -0700

    Easylist Filtering
    Proper Ad/tracker blocking 🎉🎉

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 A simple and quick patcher that blocks ads/trackers on the Blitz.gg desktop application.
 
-(currently only blocks google's doubleclick domain, for now)
-
 Using this is against Blitz.gg's terms of services. I am not responsible for what happens to your Blitz.gg account, you have been warned.
 
 ## Special thanks to
 [asar.net - .net asar archive extractor](https://github.com/Jiiks/asar.net)
+[Easylist - filters for ads/trackers](https://easylist.to/pages/about.html)
+[Cliqz' adblocker - js library for blocking ads/trackers](https://github.com/cliqz-oss/adblocker)
--- blitz-app-adblock/Program.cs
+++ blitz-app-adblock/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Text;
 using asardotnet;
+using blitz_app_adblock.Properties;
 
 namespace blitz_app_adblock {
     class Program {
@@ -8,36 +11,53 @@ namespace blitz_app_adblock {
             $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Programs\\Blitz\\resources";
         static void Main(string[] args) {
             if (File.Exists($"{appPath}\\app.asar")) {
-                Console.WriteLine($"{appPath}\\app.asar found!");
-                var asar = new AsarArchive($"{appPath}\\app.asar");
-                var extractor = new AsarExtractor();
+                Console.WriteLine("app.asar found!");
 
                 try {
                     Console.WriteLine("Extracting...");
+                    var asar = new AsarArchive($"{appPath}\\app.asar");
+                    var extractor = new AsarExtractor();
                     extractor.ExtractAll(asar, $"{appPath}\\app\\", true);
                 } catch (IOException) {
                     Console.WriteLine("Error extracting files! Make sure Blitz app is closed before trying again.");
                     Console.ReadKey();
                     return;
                 }
+            
+                Console.WriteLine("Downloading Easylists...");
+                new WebClient().DownloadFile("https://easylist.to/easylist/easylist.txt", $"{appPath}\\app\\src\\easylist.txt");
+                new WebClient().DownloadFile("https://easylist.to/easylist/easyprivacy.txt", $"{appPath}\\app\\src\\easyprivacy.txt");
 
+                Console.WriteLine("Patching...");
                 string fileToPatch = $"{appPath}\\app\\src\\createWindow.js";
 
-                Console.WriteLine(fileToPatch);
-                Console.WriteLine("Patching...");
-
+                // copy adblocker lib to src
+                File.WriteAllBytes($"{appPath}\\app\\src\\adblocker.umd.min.js", Encoding.UTF8.GetBytes(Resources.adblocker_umd_min));
+                // start writing our payload to createWindow.js
                 ModifyFileAtLine("session: true,", fileToPatch, 105);
                 ModifyFileAtLine(
 
-                    "const filter = {" +
-                    "   urls: ['*://*.doubleclick.net/*']" +
-                    "};" +
+                "try {" +
+                    "const fs = require('fs');" +
+                    "const { FiltersEngine, Request} = require('./adblocker.umd.min.js');" +
+                    "const filters = fs.readFileSync(require.resolve('./easylist.txt'), 'utf-8') + fs.readFileSync(require.resolve('./easyprivacy.txt'), 'utf-8');" +
+                    "const engine = FiltersEngine.parse(filters);" +
 
-                    "windowInstance.webContents.session.webRequest.onBeforeRequest(filter, (details, callback) => {" +
-                    "   callback({cancel: true});" +
-                    "});"
+                    "windowInstance.webContents.session.webRequest.onBeforeRequest({ urls:['*://*/*']}, (details, callback) => {" +
+                        "const { match } = engine.match(Request.fromRawDetails({ url: details.url}));" +
+                        "if (match == true) {" +
+                            "log.info('BLOCKED:', details.url);" +
+                            "callback({cancel: true});" +
+                        "} else {" +
+                            "callback({cancel: false});" +
+                        "}" +
+                    "});" +
+                "} catch (error) {" +
+                    "log.error(error);" +
+                "}"
 
                 , fileToPatch, 118);
+                // end file writes
 
                 Console.WriteLine();
                 Console.WriteLine();
