4e7ae7650849bcc147310b5409e60a00b8ea9552
lulzsun <jminquach@gmail.com>
2021-01-15 14:40:30 -0800

    Use forward slashes in file paths
    #10 unix systems use forward slashes, windows supports it too

--- blitz-app-adblock.js
+++ blitz-app-adblock.js
@@ -15,49 +15,49 @@ async function start() {
         
         // mac os app path
         if (process.platform === 'darwin') {
-            appPath = '\\Applications\\Blitz.app\\Contents\\Resources';
+            appPath = '/Applications/Blitz.app/Contents/Resources';
         }
         // windows app path
         else if (process.platform === 'win32') {
-            appPath = path.join(process.env.APPDATA, '..\\Local\\Programs\\Blitz\\resources');
+            appPath = path.join(process.env.APPDATA, '../Local/Programs/Blitz/resources');
         }
 
-        if(!appPath || !fs.existsSync(`${appPath}\\app.asar`)) {
+        if(!appPath || !fs.existsSync(`${appPath}/app.asar`)) {
             console.log("app.asar not found!");
         }
         else {
             console.log('Extracting app.asar...');
-            await asar.extractAll(`${appPath}\\app.asar`, `${appPath}\\app\\`);
+            await asar.extractAll(`${appPath}/app.asar`, `${appPath}/app/`);
         
             console.log('Downloading ad & tracking filters...');
-            await io.downloadFile('https://easylist.to/easylist/easylist.txt', `${appPath}\\app\\src\\easylist.txt`);
-            await io.downloadFile('https://easylist.to/easylist/easyprivacy.txt', `${appPath}\\app\\src\\easyprivacy.txt`);
-            await io.downloadFile('https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt', `${appPath}\\app\\src\\ublock-ads.txt`);
-            await io.downloadFile('https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt', `${appPath}\\app\\src\\ublock-privacy.txt`);
-            await io.downloadFile('https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblock&showintro=1&mimetype=plaintext', `${appPath}\\app\\src\\peter-lowe-list.txt`);
+            await io.downloadFile('https://easylist.to/easylist/easylist.txt', `${appPath}/app/src/easylist.txt`);
+            await io.downloadFile('https://easylist.to/easylist/easyprivacy.txt', `${appPath}/app/src/easyprivacy.txt`);
+            await io.downloadFile('https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt', `${appPath}/app/src/ublock-ads.txt`);
+            await io.downloadFile('https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt', `${appPath}/app/src/ublock-privacy.txt`);
+            await io.downloadFile('https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblock&showintro=1&mimetype=plaintext', `${appPath}/app/src/peter-lowe-list.txt`);
         
             console.log('Patching...');
     
             // copy adblocker lib to src
             if(fs.existsSync(`adblocker.umd.min.js`)) 
-                io.copyFile('adblocker.umd.min.js', `${appPath}\\app\\src\\adblocker.umd.min.js`)
-            else io.copyFile('./build/adblocker.umd.min.js', `${appPath}\\app\\src\\adblocker.umd.min.js`)
+                io.copyFile('adblocker.umd.min.js', `${appPath}/app/src/adblocker.umd.min.js`)
+            else io.copyFile('./build/adblocker.umd.min.js', `${appPath}/app/src/adblocker.umd.min.js`)
     
             // start writing our payload to createWindow.js
-            io.modifyFileAtLine(js.filterEngine, `${appPath}\\app\\src\\createWindow.js`, 119);
-            io.modifyFileAtLine('session: true,', `${appPath}\\app\\src\\createWindow.js`, 106);
+            io.modifyFileAtLine(js.filterEngine, `${appPath}/app/src/createWindow.js`, 119);
+            io.modifyFileAtLine('session: true,', `${appPath}/app/src/createWindow.js`, 106);
     
             // optional features
-            if (noUpdate)  io.ModifyFileAtLine('if (false) {', `${appPath}\\app\\src\\index.js`, 267);
-            if (autoGuest) io.ModifyFileAtLine(js.autoGuest, `${appPath}\\app\\src\\preload.js`, 18);
+            if (noUpdate)  io.ModifyFileAtLine('if (false) {', `${appPath}/app/src/index.js`, 267);
+            if (autoGuest) io.ModifyFileAtLine(js.autoGuest, `${appPath}/app/src/preload.js`, 18);
     
             // repack
             console.log('Repacking app.asar...');
-            await asar.createPackage(`${appPath}\\app\\`, `${appPath}\\app.asar`);
+            await asar.createPackage(`${appPath}/app/`, `${appPath}/app.asar`);
 
             // cleanup
             console.log('Cleaning up directory...');
-            io.deleteFolder(`${appPath}\\app\\`);
+            io.deleteFolder(`${appPath}/app/`);
             
             console.log('\r\nPatching complete! GLHF :)');
         }
