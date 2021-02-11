e28859191961cbbfa905ec22d383e3acc4a7ee1d
lulzsun <jminquach@gmail.com>
2021-02-11 02:05:15 -0800

    Update io.js
    Spacing set to 4 spaces instead of 2

--- io.js
+++ io.js
@@ -4,39 +4,39 @@ const https = require('https');
 const path = require('path');
 
 async function downloadFile(url, filePath) {
-  const proto = !url.charAt(4).localeCompare('s') ? https : http;
+    const proto = !url.charAt(4).localeCompare('s') ? https : http;
 
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(filePath);
-    let fileInfo = null;
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(filePath);
+        let fileInfo = null;
 
-    const request = proto.get(url, response => {
-      if (response.statusCode !== 200) {
-        reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
-        return;
-      }
+        const request = proto.get(url, response => {
+            if (response.statusCode !== 200) {
+                reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                return;
+            }
 
-      fileInfo = {
-        mime: response.headers['content-type'],
-        size: parseInt(response.headers['content-length'], 10),
-      };
+            fileInfo = {
+                mime: response.headers['content-type'],
+                size: parseInt(response.headers['content-length'], 10),
+            };
 
-      response.pipe(file);
+            response.pipe(file);
+        });
+
+        // The destination stream is ended by the time it's called
+        file.on('finish', () => resolve(fileInfo));
+
+        request.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+
+        file.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+
+        request.end();
     });
-
-    // The destination stream is ended by the time it's called
-    file.on('finish', () => resolve(fileInfo));
-
-    request.on('error', err => {
-      fs.unlink(filePath, () => reject(err));
-    });
-
-    file.on('error', err => {
-      fs.unlink(filePath, () => reject(err));
-    });
-
-    request.end();
-  });
 }
 
 function modifyFileAtLine(data, filePath, line) {
